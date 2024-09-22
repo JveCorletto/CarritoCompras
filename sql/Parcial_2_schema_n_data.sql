50040292b75f2896b11abf8222090ed0dbb3b9ee
André Martínez <est.j5martinez@gmail.com>
2024-09-21 23:48:37 -0600

    Se sube código incial del proyecto

---
+++ sql/Parcial_2_schema_n_data.sql
@@ -0,0 +1,109 @@
+CREATE DATABASE Parcial_2;
+
+USE parcial_2;
+
+CREATE TABLE Estados(
+	IdEstado INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Estado VARCHAR(255) NOT NULL
+);
+
+INSERT INTO Estados (Estado)
+VALUES 	('Activo'),
+		('Inactivo');
+
+CREATE TABLE Roles (
+    IdRol INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    IdEstado INT NOT NULL,
+    Rol VARCHAR(255) NOT NULL,
+    CONSTRAINT FK_Roles_Estados FOREIGN KEY (IdEstado) REFERENCES Estados(IdEstado)
+);
+
+INSERT INTO Roles (IdEstado, Rol)
+VALUES 	(1, 'Administrador'),
+		(1, 'Usuario');
+
+CREATE TABLE Usuarios(
+	IdUsuario BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    IdEstado INT NOT NULL,
+    IdRol INT NOT NULL,
+    Nombre VARCHAR(255) NOT NULL,
+    Usuario VARCHAR(255) NOT NULL,
+    Contrasenia VARCHAR(255) NOT NULL,
+    CONSTRAINT FK_Usuarios_Estados FOREIGN KEY (IdEstado) REFERENCES Estados(IdEstado),
+    CONSTRAINT FK_Usuarios_Roles FOREIGN KEY (IdRol) REFERENCES Roles(IdRol)
+);
+
+INSERT INTO Usuarios (IdEstado, IdRol, Nombre, Usuario, Contrasenia)
+VALUES 	(1, 1, 'Javier', 'Javier', '12345'),
+		(1, 2, 'Andre', 'Andre', '12345');
+
+CREATE TABLE Categorias(
+	IdCategoria INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Categoria VARCHAR(255) NOT NULL
+);
+
+INSERT INTO Categorias(Categoria)
+VALUES  ('Ropa'),
+        ('Zapatos'),
+        ('Electrodomésticos');
+
+CREATE TABLE Productos(
+    IdProducto BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    IdCategoria INT NOT NULL,
+    Producto VARCHAR(255) NOT NULL,
+    Descripcion VARCHAR(255) NOT NULL,
+    Imagen BLOB NULL,
+    Precio DOUBLE(14,2) NOT NULL,
+    Stock INT NOT NULL,
+
+    -- CAMPOS DE AUDITORIA
+    UsuarioCreacion VARCHAR(255) NOT NULL,
+    FechaCreacion DATE NOT NULL,
+    UsuarioModificacion VARCHAR(255) NULL,
+    FechaModificacion DATE NULL,
+
+    CONSTRAINT FK_Productos_Categorias FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria)
+);
+
+CREATE TABLE EstadosCompras (
+    IdEstadoCompra INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    EstadoCompra VARCHAR(255) NOT NULL
+);
+
+INSERT INTO EstadosCompras(EstadoCompra)
+VALUES  ('Pendiente de pago'),
+        ('Pagada');
+
+CREATE TABLE Compras(
+    IdCompra BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    IdEstadoCompra INT NOT NULL,
+    IdComprobante BIGINT NOT NULL,
+    FechaCompra DATE NOT NULL,
+    UsuarioCompra BIGINT NOT NULL,
+    TotalCompra DOUBLE(14,2) NOT NULL,
+
+    CONSTRAINT FK_Compras_EstadosCompras FOREIGN KEY (IdEstadoCompra) REFERENCES EstadosCompras(IdEstadoCompra),
+    CONSTRAINT FK_Compras_Usuarios FOREIGN KEY (UsuarioCompra) REFERENCES Usuarios(IdUsuario)
+);
+
+CREATE TABLE DetallesCompras(
+    IdDetalleCompra BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    IdCompra BIGINT NOT NULL,
+    IdProducto BIGINT NOT NULL,
+    Cantidad INT NOT NULL,
+    PrecioUnitario DOUBLE(14,2) NOT NULL,
+    SubTotal DOUBLE(14,2) NOT NULL,
+
+    CONSTRAINT FK_DetallesCompras_Compras FOREIGN KEY (IdCompra) REFERENCES Compras(IdCompra),
+    CONSTRAINT FK_DetallesCompras_Productos FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto)
+);
+
+CREATE TABLE ComprobantesCompras(
+    IdComprobante BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    IdCompra BIGINT NOT NULL,
+    Tarjeta VARCHAR(15) NOT NULL,
+    TokenComprobante VARCHAR(255) NOT NULL,
+    FechaTransaccion DATETIME NOT NULL,
+
+    CONSTRAINT FK_ComprobantesCompras_Compras FOREIGN KEY (IdCompra) REFERENCES Compras(IdCompra)
+);
---
+++ src/test/java/com/ufg/parcial_2/Parcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufg.parcial_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
